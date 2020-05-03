f9d8fc4c326cc49053052da7037c7c1af5ac4759
andrews balbino campos <andrews.campos@hotmail.com>
2020-05-03 19:05:49 -0300

    first commit

---
+++ src/main/java/com/example/demo/ImportContact1Application.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ImportContact1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportContact1Application.class, args);
+	}
+	
+	ArrayList<Object> list = new ArrayList<>();
+	
+	ArrayList<Object> status = new ArrayList<>();
+	
+	@RequestMapping(value = "/reciver", method = RequestMethod.POST)
+	public void reciver(@RequestBody Object obs) {
+		list.add(obs);
+	}
+	
+	
+	@RequestMapping(value = "/reciver", method = RequestMethod.GET)
+	@ResponseBody
+	public ArrayList<Object> getReciver() {
+		return list;
+	}
+	
+	@RequestMapping(value = "/status", method = RequestMethod.POST)
+	public void status(@RequestBody Object obs) {
+		status.add(obs);
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/status", method = RequestMethod.GET)
+	public ArrayList<Object> getStatus() {
+		return status;
+	}
+}
